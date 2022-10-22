822999c64cfa9464722dd40e41a4941170de6291
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-10-22 17:58:23 +0300

    finalized 1,2

--- main.cpp
+++ main.cpp
@@ -2,12 +2,43 @@
 #include <windows.h>
 using namespace std;
 
+struct VectorXY {
+    float x = 0.0;
+    float y = 0.0;
+};
+
+string getCommand () {
+    string command;
+    cout << "\n¬ведите команду (add | sub | scale | length | normalize | quit): ";
+    cin >> command;
+    for (char & i : command) {
+        i = (char) tolower(i);
+    }
+    return command;
+}
+
+VectorXY getVectorInput () {
+    VectorXY a;
+    cout << "¬ведите координаты вектора (x,y) через пробел: ";
+    cin >> a.x >> a.y;
+    return a;
+}
+
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "RUS");
 
+    string comm;
+    do {
+        comm = getCommand();
+        if (comm == "add") {
+            VectorXY a = getVectorInput();
+        }
 
+
+    } while (comm != "quit");
 
 
     return 0;
